15d449f49eb5169984e43036d706f77794f60165
Franco Pertusati <pertusatifranco836@gmail.com>
2025-08-21 23:58:35 -0300

    Implementacion de cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const swaggerUI = require("swagger-ui-express");
 const OpenApiValidator = require("express-openapi-validator");
 const routes = require("./routes");
+const cors = require("cors")
 
 const YAML = require("yamljs");
 swaggerDocument = YAML.load("./openapi.yaml");
@@ -9,6 +10,7 @@ swaggerDocument = YAML.load("./openapi.yaml");
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use(
   OpenApiValidator.middleware({
