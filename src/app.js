1d882a02d442cd0af3475d11461d17b8c01d44f4
Franco Pertusati <pertusatifranco836@gmail.com>
2025-09-12 20:41:40 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: "https://linktun25.vercel.app/",
     credentials: true,
   })
 );
